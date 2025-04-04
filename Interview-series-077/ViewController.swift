c2b80f09a4a2e872f0187836c934506f52be0c6f
poyt-cg-aes-jur <206003202+poyt-cg-aes-jur@users.noreply.github.com>
2025-04-03 20:56:09 -0400

    [ADD] integrate api

--- Interview-series-077/ViewController.swift
+++ Interview-series-077/ViewController.swift
@@ -7,7 +7,11 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    private let baseURL = "https://fileupload.rick-and-friends.site/search?keyword="
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
@@ -28,8 +32,9 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         searchTask?.cancel()
-        searchTask = DispatchWorkItem {
+        searchTask = DispatchWorkItem { [weak self] in
             print("making API call")
+            guard let self = self else { return }
             self.fetchResults(for: searchText)
         }
         
@@ -42,12 +47,13 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
         guard !query.isEmpty else { return }
         currentTask?.cancel()
         
+        //TODO: - implement logic
         guard let apiKey = UserDefaults.standard.string(forKey: "apiKey") else {
             print("Error: API Key is missing")
             return
         }
         
-        let urlString = "https://mockapi.example.com/search?q=\(query)&apiKey=\(apiKey)"
+        let urlString = baseURL + query
         
         guard let url = URL(string: urlString) else { return }
         
@@ -55,9 +61,12 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
             guard let data = data, error == nil else { return }
             
             do {
-                let decodedResponse = try JSONDecoder().decode([String].self, from: data)
-                self.results = decodedResponse
-                self.tableView.reloadData()
+                let decodedResponse = try JSONDecoder().decode(Response.self, from: data)
+                self.results = decodedResponse.results.map{ $0.key }
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
+                    self.tableView.reloadData()
+                }
             } catch {
                 print("Decoding error: \(error)")
             }
@@ -78,5 +87,17 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
 }
 
 
+struct Response: Codable {
+    let success: Bool
+    let count: Int
+    let results: [Result]
+}
 
+// MARK: - Result
+struct Result: Codable {
+    let key: String
+    let size: Int
+    let uploaded: String
+    let url: String
+}
 
